c432b6c08377ab19151c2443fcfae97abebb5911
seiya kato <sablier6969@yahoo.co.jp>
2020-11-10 17:12:38 +0900

    recommit

--- components/Header.vue
+++ components/Header.vue
@@ -29,7 +29,7 @@
             </v-menu>
             <v-container>
                 <v-toolbar-items
-                class="d-xs-none nav">
+                class="d-sm-none nav">
                     <v-row
                     align="center"
                     justify="center"
